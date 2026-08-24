f36fc653097bd0811614bbbb86967f97668e68e5
KETAVATHDILESWAR <dileswarketavath2002@gmail.com>
2026-08-24 14:34:10 +0530

    Change S3 backend region to us-east-2

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -8,7 +8,7 @@ terraform {
   backend "s3" {
     bucket       = "banking-platform-tfstate-118178010323" # create once, then set here
     key          = "dev/networking.tfstate"
-    region       = "ap-south-2"
+    region       = "us-east-2"
     use_lockfile = true
     encrypt      = true
   }
